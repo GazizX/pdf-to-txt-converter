d8a2b8113040e728f1ad949ea93711cece113aeb
str1xoid <134718130+str1xoid@users.noreply.github.com>
2025-06-07 17:34:48 +0300

    implemented error handling in pdf upload

--- backend/python/converter/server.py
+++ backend/python/converter/server.py
@@ -10,10 +10,21 @@ tasks_queue = Queue()
 
 class PDFConverterServicer(pdf_converter_pb2_grpc.PDFConverterServicer):
     def UploadPDF(self, request_iterator, context):
-        with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as tmp:
-            for chunk in request_iterator:
-                tmp.write(chunk.data)
-            tmp_path = tmp.name
+        try:
+            # creating a new tmp .pdf file
+            with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as tmp:
+                # collecting pdf data & wrighting in tmp
+                for chunk in request_iterator:
+                    if not chunk.data:
+                        raise ValueError("Empty chunk received")
+                    tmp.write(chunk.data)
+                tmp_path = tmp.name
 
-        tasks_queue.put(tmp_path)
-        return pdf_converter_pb2.UploadResponce(success=True)
+            #adding a file to a tasks queue
+            tasks_queue.put(tmp_path)
+            return pdf_converter_pb2.UploadResponce(success=True)
+        
+        except Exception as e:
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(f"Error: {str(e)}")
+            return pdf_converter_pb2.UploadResponce(success=False)
